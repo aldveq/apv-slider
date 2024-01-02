3fe3a4ca38de9b9acb951bdef08c2016fcab8da6
Aldo Paz Velasquez <aldveq93@Aldos-Mac-mini.local>
2024-01-01 20:54:20 -0600

    first commit

---
+++ avp-slider.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Plugin Name:       AVP Slider
+ * Plugin URI:        https://wordpress.org/plugins/avp-slider
+ * Description:       Free WordPress Slider Plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Aldo Paz Velasquez
+ * Author URI:        https://apazvelasquez.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       avp-slider
+ * Domain Path:       /languages
+ */
+
+ /*
+AVP Slider is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+AVP Slider is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with AVP Slider. If not, see {URI to Plugin License}.
+*/
+
+if ( ! defined( 'ASPATH' ) ):
+	exit;
+endif;
